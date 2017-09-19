517ae6642822c4d426f9da3d63378b1d3858e74c
Pamela Frieze <pfrieze@gmail.com>
2017-09-19 18:45:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(integer)
   while integer >0
-    puts "#{number} SECOND(S)!"
+    puts "#{integer} SECOND(S)!"
   until integer == 0
     puts "HAPPY NEW YEAR!"
 integer == integer -= 1
